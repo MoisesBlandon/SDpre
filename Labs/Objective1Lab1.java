be286bdf8435a9be7a0f9377de31cd6f9d1e3a4d
“MoisesBlandon” <Moisesblandon@outlook.com>
2023-10-19 15:46:45 -0400

    complete objective1lab1

--- Labs/Objective1Lab1.java
+++ Labs/Objective1Lab1.java
@@ -1,7 +1,5 @@
 public class Objective1Lab1 {
     public static void main (String[] args) {
-      /*
-        System.out.print.ln("Hello, Skill Distillery");
-      */
+        System.out.println("Hello, Skill Distillery"); 
     }
 }
