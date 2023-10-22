52d6148862557a4d967ee0e9747227fd46dd2a2f
“MoisesBlandon” <Moisesblandon@outlook.com>
2023-10-22 03:31:20 -0400

    Complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,12 @@
+public class Objective3Lab2 {
+	public static void main (String[] args) {
+		Double side1, side2, side3, hypotenuse;
+
+		side1 = 10.0;
+		side2 = 8.0;
+		hypotenuse = (side1*side1) + (side2*side2);
+
+		System.out.println(Math.sqrt(hypotenuse));
+
+	}
+}
